4654b9d9e2cfe6a64edc6e9657cd5500dc9a2781
RedSkull_7_7 <icaro.nata147@gmail.com>
2024-09-02 13:35:02 -0300

    Fist commit

---
+++ src/CadPessoa/PessoaFisica.java
@@ -0,0 +1,13 @@
+package CadPessoa;
+
+public class PessoaFisica extends Pessoa {
+	private Long cpf;
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+}
---
+++ src/CadPessoa/PessoaJuridica.java
@@ -0,0 +1,13 @@
+package CadPessoa;
+
+public class PessoaJuridica extends Pessoa {
+	private Long cnpj;
+
+	public Long getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(Long cnpj) {
+		this.cnpj = cnpj;
+	}
+}
